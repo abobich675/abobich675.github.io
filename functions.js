5d1d3025afba1829928bc30f76b0e69f34f9327e
Adam <abobich675@gmail.com>
2023-10-02 16:03:10 -0700

    update

--- functions.js
+++ functions.js
@@ -1,3 +1,37 @@
+function roll(count, size) {
+    var roll = 0;
+    for (let i = 0; i < count; i++) {
+        let addRoll = Math.floor((Math.random() * size) + 1);
+        roll += addRoll;
+    }
+    return roll;
+}
+
+function changeText() {
+    var element = document.getElementById("greatsword");
+    element.innerHTML = "Hello World!";
+}
+
+//function addText(element, text) {
+//    element = document.getElementById(element);
+//    element.innerHTML += "<br>" + text;
+//}
+
+function addText(element, text) {
+    const para = document.createElement("p");
+    const node = document.createTextNode(text);
+    para.appendChild(node);
+
+    element = document.getElementById(element);
+    element.appendChild(para);
+}
+
+
+function output(text) {
+    addText('feedback', text);
+}
+
+
 function weapon(dmg, mod) {
     var ac = document.getElementById("AC").value;
     if (isNaN(ac) || ac == "") {
@@ -12,12 +46,13 @@ function weapon(dmg, mod) {
     var damage = dmg + mod;
 
     if (success) {
-        alert("You rolled a " + toHit + " which beats your enemy's " + ac + " Armor Class\nYou deal " + damage + " damage!");
-    } else { alert("You rolled a " + toHit + " which misses your enemy's " + ac + " Armor Class"); }
+        output("You rolled a " + toHit + " which beats your enemy's " + ac + " Armor Class\nYou deal " + damage + " damage!");
+    } else { output("You rolled a " + toHit + " which misses your enemy's " + ac + " Armor Class"); }
 }
 
 function greatsword(mod) {
 
+    changeText();
     var ac = document.getElementById("AC").value;
     if (isNaN(ac) || ac == "") {
         alert("You must enter a valid Armor Class!");
@@ -31,17 +66,8 @@ function greatsword(mod) {
     var damage = roll(2,6) + mod;
 
     if (success) {
-        alert("You rolled a " + toHit + " which beats your enemy's " + ac + " Armor Class\nYou deal " + damage + " damage!");
-    } else { alert("You rolled a " + toHit + " which misses your enemy's " + ac + " Armor Class"); }
+        output("You rolled a " + toHit + " which beats your enemy's " + ac + " Armor Class\nYou deal " + damage + " damage!");
+    } else { output("You rolled a " + toHit + " which misses your enemy's " + ac + " Armor Class"); }
 
 
-}
-
-function roll(count, size) {
-    var roll = 0;
-    for (let i = 0; i < count; i++) {
-        let addRoll = Math.floor((Math.random() * size) + 1);
-        roll += addRoll;
-    }
-    return roll;
 }
