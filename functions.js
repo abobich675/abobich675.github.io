53c9fda643b7b77c72e163b74d27ae3cdd8cfa1e
Adam <abobich675@gmail.com>
2023-10-04 13:43:21 -0700

    Noah

--- functions.js
+++ functions.js
@@ -125,7 +125,7 @@ function createGrid(data) {
     weapons = data;
     for (let i = 0; i < weapons["weapons"].length; i++) {
         var position = "r" + (Math.floor(i / columns) + 1) + "c" + ((i + 1) % columns);
-        console.log(position + "test");
+        console.log(position);
         var label = weapons["weapons"][i]["name"] + ": " + weapons["weapons"][i]["damage"]["count"] + "d" + weapons["weapons"][i]["damage"]["size"];
         if (weapons["weapons"][i]["bonus"] != 0) label += " + " + weapons["weapons"][i]["bonus"];
         changeText(position, label);
@@ -134,8 +134,10 @@ function createGrid(data) {
     //if (weapons["weapons"][0]["bonus"] != 0) label += " + " + weapons["weapons"][0]["bonus"];
     //changeText(position, label);
 
-    const row = document.createElement("div class='row'");
-    const column = document.createElement("div class='column'");
+    const row = document.createElement("div");
+    row.classList.add("row");
+    const column = document.createElement("div");
+    column.classList.add("column");
     const node = document.createTextNode("hello");
     row.appendChild(column);
     column.appendChild(node);
