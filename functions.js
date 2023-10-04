e103d3d6947fc71cf78a2e5a973041acfb14255a
Adam <abobich675@gmail.com>
2023-10-04 15:20:57 -0700

    Hank

--- functions.js
+++ functions.js
@@ -144,7 +144,7 @@ function createGrid(data) {
         }
         const column = document.createElement("div");
         column.classList.add("column");
-        column.style.backgroundColor = red;
+        column.style.backgroundColor = #aaa;
 
         const par = document.createElement("p");
 
