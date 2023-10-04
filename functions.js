0af15bed5872d5db4a78c2fbfed99acdd7f05ca8
Adam <abobich675@gmail.com>
2023-10-04 16:30:29 -0700

    Jerome

--- functions.js
+++ functions.js
@@ -144,7 +144,7 @@ function createGrid(data) {
         }
         const column = document.createElement("div");
         column.classList.add("column");
-        column.style.backgroundColor = "rgba(0,0,0," + ((i % columns) * Math.floor(i/columns))/ 25 + ")";
+        column.style.backgroundColor = "rgba(0,0,0," + (((i % columns) + 1) * Math.floor(i/columns + 1))/ 25 + ")";
 
         const par = document.createElement("p");
 
@@ -209,4 +209,34 @@ function getMod(weapon) {
         return;
     }
     return mod;
+}
+
+function testFunction() {
+    const fs = require('fs');
+
+    // Read the contents of the JSON file
+    const data = fs.readFileSync('data.json');
+    // Parse the JSON data into a JavaScript object
+    const jsonData = JSON.parse(data);
+
+    console.log("Before Adding data", JSON.stringify(jsonData, null, 4));
+
+    // Modify the JavaScript object by adding new data
+    jsonData.users.push({
+        name: "James Den",
+        email: "james.den@example.com"
+    });
+
+
+    // Convert the JavaScript object back into a JSON string
+    const jsonString = JSON.stringify(jsonData);
+
+    fs.writeFileSync('data.json', jsonString, 'utf-8', (err) => {
+        if (err) throw err;
+        console.log('Data added to file');
+    });
+
+    const update_data = fs.readFileSync('data.json');
+    const updated_jsonData = JSON.parse(update_data);
+    console.log("After Adding data", JSON.stringify(updated_jsonData, null, 4));
 }
