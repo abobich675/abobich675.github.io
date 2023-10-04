74a75c0cd21e4f728be43f829f4019b5c527bb99
Adam <abobich675@gmail.com>
2023-10-04 09:54:31 -0700

    Heather

--- functions.js
+++ functions.js
@@ -66,9 +66,6 @@ function weapon(dmg, mod) {
 }
 
 function attack(weapon) {
-    var modType = weapons["weapons"][0]["modifier"];
-    var str = document.getElementById("str").value * 1;
-    var dex = document.getElementById("dex").value * 1;
     var count = weapons["weapons"][0]["damage"]["count"];
     var size = weapons["weapons"][0]["damage"]["size"];
     var ac = document.getElementById("AC").value;
@@ -78,37 +75,10 @@ function attack(weapon) {
     var nat1 = false;
     var success;
     var damage;
-    var mod;
+    var mod = getMod();
 
     if (isNaN(ac) || ac == "") {
         alert("You must enter a valid Armor Class!");
-        return;
-    }
-
-    if (modType == "strength") {
-        if (isNaN(str) || str == "") {
-            alert("You must enter a valid strength modifier!");
-            return;
-        } else mod = str;
-    } else if (modType == "dexterity") {
-        if (isNaN(dex) || dex == "") {
-            alert("You must enter a valid dexterity modifier!");
-            return;
-        } else mod = dex;
-    } else if (modType == "finesse") {
-        if (isNaN(str) || str == "" && isNaN(dex) || dex == "") {
-            alert("You must enter a valid strength or dexterity modifier!");
-            return;
-        } else if (isNaN(str) || str == "") mod = dex;
-        else if (isNaN(dex) || dex == "") mod = str;
-        else if (str >= dex) mod = str;
-        else if (dex > str) mod = dex;
-        else {
-            output("An error has occured (strength or dexterity score invalid)");
-            return;
-        }
-    } else {
-        output("An error has occured (modifier named identified)");
         return;
     }
 
@@ -152,12 +122,49 @@ function greatsword(mod) {
 function createGrid(data) {
     weapons = data;
     var label = weapons["weapons"][0]["name"] + ": " + weapons["weapons"][0]["damage"]["count"] + "d" + weapons["weapons"][0]["damage"]["size"];
-    if (weapons["weapons"][0]["bonus"] != 0) weapons += " + " + weapons["weapons"][0]["bonus"];
+    if (weapons["weapons"][0]["bonus"] != 0) label += " + " + weapons["weapons"][0]["bonus"];
     changeText('r1c1', label);
 }
 
 function refreshGrid() {
     var label = weapons["weapons"][0]["name"] + ": " + weapons["weapons"][0]["damage"]["count"] + "d" + weapons["weapons"][0]["damage"]["size"];
-    if (weapons["weapons"][0]["bonus"] != 0) weapons += " + " + weapons["weapons"][0]["bonus"];
+    var mod = getMod();
+    if (weapons["weapons"][0]["bonus"] != 0) mod += weapons["weapons"][0]["bonus"];
+        label += " + " + mod;
     changeText('r1c1', label);
+}
+
+function getMod(weapon) {
+    var modType = weapon["modifier"];
+    var str = document.getElementById("str").value * 1;
+    var dex = document.getElementById("dex").value * 1;
+    var mod;
+
+    if (modType == "strength") {
+        if (isNaN(str) || str == "") {
+            alert("You must enter a valid strength modifier!");
+            return;
+        } else mod = str;
+    } else if (modType == "dexterity") {
+        if (isNaN(dex) || dex == "") {
+            alert("You must enter a valid dexterity modifier!");
+            return;
+        } else mod = dex;
+    } else if (modType == "finesse") {
+        if (isNaN(str) || str == "" && isNaN(dex) || dex == "") {
+            alert("You must enter a valid strength or dexterity modifier!");
+            return;
+        } else if (isNaN(str) || str == "") mod = dex;
+        else if (isNaN(dex) || dex == "") mod = str;
+        else if (str >= dex) mod = str;
+        else if (dex > str) mod = dex;
+        else {
+            output("An error has occured (strength or dexterity score invalid)");
+            return;
+        }
+    } else {
+        output("An error has occured (modifier named identified)");
+        return;
+    }
+    return mod;
 }
