74b1aee168c984823b34bbb4e11b623b3be7c0f0
Adam <abobich675@gmail.com>
2023-10-04 16:54:01 -0700

    Lilith

--- functions.js
+++ functions.js
@@ -1,5 +1,3 @@
-import * as fs from 'fs';
-
 var weapons;
 var rows = 1;
 var columns = 3;
@@ -215,31 +213,4 @@ function getMod(weapon) {
 }
 
 function testFunction() {
-    const fs = require('fs');
-
-    // Read the contents of the JSON file
-    const data = fs.readFileSync('data.json');
-    // Parse the JSON data into a JavaScript object
-    const jsonData = JSON.parse(data);
-
-    console.log("Before Adding data", JSON.stringify(jsonData, null, 4));
-
-    // Modify the JavaScript object by adding new data
-    jsonData.users.push({
-        name: "James Den",
-        email: "james.den@example.com"
-    });
-
-
-    // Convert the JavaScript object back into a JSON string
-    const jsonString = JSON.stringify(jsonData);
-
-    fs.writeFileSync('data.json', jsonString, 'utf-8', (err) => {
-        if (err) throw err;
-        console.log('Data added to file');
-    });
-
-    const update_data = fs.readFileSync('data.json');
-    const updated_jsonData = JSON.parse(update_data);
-    console.log("After Adding data", JSON.stringify(updated_jsonData, null, 4));
 }
