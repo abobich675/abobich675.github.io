a53fe79c6d5cd266d9588d49ed6003b4b04d46c5
Adam <abobich675@gmail.com>
2023-10-03 17:16:42 -0700

    change

--- functions.js
+++ functions.js
@@ -1,3 +1,7 @@
+$.getJSON("weapons.json", function (json) {
+    console.log(json); // this will show the info it in firebug console
+});
+
 function roll(count, size) {
     var roll = 0;
     for (let i = 0; i < count; i++) {
@@ -34,20 +38,33 @@ function output(text) {
 
 function weapon(dmg, mod) {
     var ac = document.getElementById("AC").value;
+    var critRule = document.getElementById("nat20").checked;
+    var toHit = Math.floor((Math.random() * 20) + 1)
+    var nat20 = false;
+    var nat1 = false;
+    var success;
+    var damage;
+
     if (isNaN(ac) || ac == "") {
         alert("You must enter a valid Armor Class!");
         return;
     }
-    var toHit = Math.floor((Math.random() * 20) + 1) + mod;
-    if (toHit >= ac) {
-        var success = true;
-    } else { var success = false; }
 
-    var damage = dmg + mod;
+    if (toHit == 20) nat20 = true;
+    else if (toHit == 1) nat1 = true;
+    else toHit += mod;
 
-    if (success) {
-        output("You rolled a " + toHit + " which beats your enemy's " + ac + " Armor Class\nYou deal " + damage + " damage!");
-    } else { output("You rolled a " + toHit + " which misses your enemy's " + ac + " Armor Class"); }
+    if (critRule && nat20) success = true;
+    else if (toHit >= ac) success = true;
+    else success = false;
+
+    damage = dmg + mod;
+    if (nat20) damage += dmg;
+
+    if (nat20 && critRule) output("You rolled a Natural 20!!!\nYou deal " + damage + " damage!");
+    else if (nat1 && critRule) output("You rolled a Natural 1.\nLoser.");
+    else if (success) output("You rolled a " + toHit + " which beats your enemy's " + ac + " Armor Class\nYou deal " + damage + " damage!");
+    else output("You rolled a " + toHit + " which misses your enemy's " + ac + " Armor Class");
 }
 
 function greatsword(mod) {
@@ -66,4 +83,10 @@ function greatsword(mod) {
     if (success) {
         output("You rolled a " + toHit + " which beats your enemy's " + ac + " Armor Class\nYou deal " + damage + " damage!");
     } else { output("You rolled a " + toHit + " which misses your enemy's " + ac + " Armor Class"); }
+}
+
+async function getFile() {
+    
+    output(1);
+    
 }
