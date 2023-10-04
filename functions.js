49157e6621098b5cfd7cddec5baf1e48009de715
Adam <abobich675@gmail.com>
2023-10-03 18:17:28 -0700

    test

--- functions.js
+++ functions.js
@@ -84,7 +84,7 @@ function greatsword(mod) {
 function createGrid(data) {
 
 
-    var label = data["weapons"][0]["name"] + ": " + data["weapons"][0]["damage"][0] + "d" + data["weapons"][0]["damage"][1];
+    var label = data["weapons"][0]["name"] + ": " + data["weapons"][0]["damage"]["count"] + "d" + data["weapons"][0]["damage"]["size"];
     if (data["weapons"][0]["bonus"] != 0) data += " + " + data["weapons"][0]["bonus"];
     changeText('r1c1',label);
 }
