71a278bee2446856aea073d74917ef498eeb3d7d
Adam <abobich675@gmail.com>
2023-10-13 15:37:41 -0700

    Teddy

--- functions.js
+++ functions.js
@@ -35,7 +35,7 @@ function createGrid() {
         var red = (columns - ((i % columns) + 1)) * multiplier;
         var green = 0; //(Math.floor(i / columns) + 1) * 20;
         // multiplier*columns-Abs(red-blue)
-        var blue = ((i % columns) + 1) * multiplier; // (red) * (green) / 20;
+        var blue = (i % columns) * multiplier; // (red) * (green) / 20;
         if (((Math.floor((i + 1) / columns)) + ((i + 1) % columns)) % 2 == 0) var alpha = 0.4;
         else var alpha = 0.5;
         column.style.backgroundColor = "rgba(" + red + "," + green + "," + blue + "," + alpha + ")";
@@ -234,8 +234,10 @@ function advantageChanged(setting) {
 
 // Changes the number of columns in the weapon grid and refreshes the grid
 function columnNumber(difference) {
-    columns += difference;
-    console.log("Grid columns changed by" + difference);
+    if (columns += difference > 0) {
+        columns += difference;
+        console.log("Grid columns changed by" + difference);
+    } else console.log("Could not change grid columns by" + difference);
     refreshGrid();
 }
 
